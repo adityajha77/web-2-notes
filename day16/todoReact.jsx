fe183580b42eda4bd6424a2d5653f9f4189e9442
ADITYA KUMAR JHA <adityakartikey1259@gmail.com>
2026-01-27 12:37:29 +0530

    day16 updated
    added todo

--- day16/todoReact.jsx
+++ day16/todoReact.jsx
@@ -1,3 +1,39 @@
 // do same as linkdin for todo and use fetch and take data from the jsonplaceholder website
 //use loading also stat variable
 //and the tab data also
+
+
+
+import { useState } from "react"
+
+function App(){
+    const [currentTab,setCurrentTab]=useState(null);
+    async function getTodo(id){
+        const res= await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+        const data= await res.json();
+            setCurrentTab(data);
+    }
+    
+
+
+    return (
+        <div style={{padding:20}}>
+            
+                <button onClick={()=>getTodo(1)}>Todo1</button>
+                <button onClick={()=>getTodo(2)}>Todo2</button>
+                <button onClick={()=>getTodo(3)}>Todo3</button>
+                <button onClick={()=>getTodo(4)}>Todo4</button>
+            {currentTab && (
+                <div style={{marginTop:20}}>
+                    <p>TITLE={currentTab.title}</p>
+                    <p>Id={currentTab.id}</p>
+                    <p>USERID={currentTab.userId}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default App;
+
+
